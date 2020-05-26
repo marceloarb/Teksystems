8b6e057299f2e1bc3c1d238b16f6c1a36de6f2d2
marceloarb <59586505+marceloarb@users.noreply.github.com>
2020-05-25 19:22:46 -0500

    Saving exercises

---
+++ exercise2.js
@@ -0,0 +1,55 @@
+// PROBLEM 1:
+// const Dog = class{
+//     constructor(breed,color,age){
+//         this.breed = breed;
+//         this.color = color;
+//         this.age = age;
+//     }
+// }
+
+// Dog.prototype.ageInHumanYears = function(){
+//     console.log(this.age * 7)
+// };
+// const dog1 = new Dog("Labrador","Brown",3);
+// const dog2 = new Dog("Pitbull", "white", 5);
+// dog1.ageInHumanYears();
+// dog2.ageInHumanYears();
+
+// ------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// PROBLEM 2:
+// const Dog = {
+//     ageInHumanYears: function(){
+//         console.log(this.age * 7);
+//     }
+// };
+
+// const dog1 = Object.create(Dog);
+// dog1.breed = "Lab";
+// dog1.color = "Brown";
+// dog1.age = 4;
+// console.log(dog1);
+// dog1.ageInHumanYears();
+
+
+
+
+// ------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+// PROBLEM 3:
+
+
+
+
+
+
+
+
+
+// ------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+// PROBLEM 4:
